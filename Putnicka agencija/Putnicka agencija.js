bac1f0b3315dbd40d2f676f3f5a088bb17722eb8
Lucija Posavec <lucijaposavec119@gmail.com>
2026-09-15 17:15:26 +0200

    Add files via upload

---
+++ Putnicka agencija/Putnicka agencija.js
@@ -0,0 +1,13 @@
+function pozdrav() {
+    var ime = prompt("Upiši ime i prezime:");
+
+    alert("Hej, " + ime + "! Dobrodošli u našu agenciju.");
+}
+
+function zamijeniBoju() {
+    document.body.style.backgroundColor = "lightblue";
+}
+
+function pokaziPoruku() {
+    alert("Dobro si kliknuo!");
+}
